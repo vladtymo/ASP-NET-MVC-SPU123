cc5ca8c087e3efe13604c17900073e92e0e6f7a5
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-11 10:29:09 +0200

    Add project files.

---
+++ SPU123_Shop_MVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPU123_Shop_MVC.Models;
+using System.Diagnostics;
+
+namespace SPU123_Shop_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // ...code...
+            return View(); // ~/Home/Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SPU123_Shop_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPU123_Shop_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
